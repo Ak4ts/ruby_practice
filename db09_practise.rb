0f17b9d3ec13d5616e599d4b85b0febcb5e182cc
Luiz Guilherme de Melo <80287220+Ak4ts@users.noreply.github.com>
2022-08-24 18:55:51 -0300

    all exercices from today

--- db09_practise.rb
+++ db09_practise.rb
@@ -4,8 +4,12 @@ require_relative 'methods.rb'
 
 client = Mysql2::Client.new(host: "db09.blockshopper.com", username: ENV['DB09_LGN'], password: ENV['DB09_PWD'], database: "applicant_tests")
 
-puts get_subject_teacher('2', client)
-puts get_class_subjects('Crases', client)
-puts get_teachers_list_by_letter('a', client)
+# puts get_subject_teacher('2', client)
+# puts get_class_subjects('Crases', client)
+# puts get_teachers_list_by_letter('a', client)
+
+# set_md5(client)
+# puts get_class_info(2, client)
+# puts get_teachers_by_year(2000, client)
 
 client.close
--- methods.rb
+++ methods.rb
@@ -1,10 +1,12 @@
+require 'digest'
+
 def get_subject_teacher(id, client)
   q = "select s.name subject, t.* from subjects_luiz s JOIN teachers_luiz t ON s.id = t.subject_id where s.id = #{id};"
   data = client.query(q).to_a
   if data.length != 0
     output = "Subject: #{data[0]['subject']}\nTeachers:"
     data.each do |teacher|
-      output+="\n#{teacher['first_name']} #{teacher['middle_name'][0]}. #{teacher['last_name']}"
+      output+="\n#{teacher['first_name']} #{teacher['middle_name']}. #{teacher['last_name']}"
     end
   else
     output="Can't find or teachers or subject."
@@ -39,4 +41,42 @@ def get_teachers_list_by_letter(l, client)
     end
   end
   output
+end
+
+def set_md5(client)
+  q = "select id, concat(t.first_name, t.middle_name, t.last_name, t.birth_date, t.subject_id, t.current_age) as teacher from teachers_luiz t"
+  teachers = client.query(q).to_a
+  teachers.each do |teacher|
+    md5 = Digest::MD5.hexdigest teacher['teacher']
+    client.query("UPDATE teachers_luiz SET md5 = '#{md5}' WHERE id=#{teacher['id']}")
+  end
+end
+
+def get_class_info(id, client)
+  r = "SELECT c.name, concat(t.first_name, ' ', t.middle_name, ' ', t.last_name) as full_name, concat(t2.first_name, ' ', t2.middle_name, ' ', t2.last_name) as resp
+  FROM teachers_classes_luiz tc
+  JOIN teachers_luiz t 
+    ON t.id=tc.teacher_id
+  JOIN classes_luiz c
+    ON c.id = #{id}
+  JOIN teachers_luiz t2
+    ON t2.id = c.responsible_teacher_id
+  WHERE tc.class_id=#{id}"
+
+  classt = client.query(r).to_a
+  srt = "Class Name: #{classt[0]['name']}\nResponsible Teacher: #{classt[0]['resp']}\nInvolved teachers: "
+  classt.each do |t|
+    srt += "#{t['full_name']}; "
+  end
+  srt
+end
+
+def get_teachers_by_year(year, client)
+  q = "SELECT CONCAT(t.first_name, ' ', t.middle_name, ' ', t.last_name) as full_name FROM teachers_luiz t WHERE YEAR(birth_date) = #{year}"
+  teachers = client.query(q).to_a
+  str = "Teachers born in #{year}: "
+  teachers.each do |teacher|
+    str+= "#{teacher['full_name']}; "
+  end
+  str
 end
